52c40364d411e99690fd8e7631687c9384c602b7
Luis Luna <103229493+kill-samurai@users.noreply.github.com>
2022-07-22 14:00:23 -0400

    Add files via upload
    Added EXE for Windows and New APP file for mac

--- logic.py
+++ logic.py
@@ -1,7 +1,6 @@
 import pytube
 from tkinter import messagebox
 import os
-from sys import platform
 
 
 class YouTube:
@@ -11,17 +10,18 @@ class YouTube:
         self.yt = pytube.YouTube(url=self.url, use_oauth=False, allow_oauth_cache=True)
         self.video_title = self.yt.title
         self.audio_only = self.mp3
+        self.path = path
         os.chdir(path)
-
         self.check_vid_or_audio()
 
     def check_vid_or_audio(self):
-        if self.audio_only:
+        if self.audio_only == "1":
             is_this_the_one = messagebox.askquestion(message=f"Is this the video name: \n{self.video_title}")
             if is_this_the_one == "yes":
                 streams = self.yt.streams.filter(only_audio=True, mime_type="audio/mp4")
                 download = streams.last()
                 download.download()
+
         else:
             self.check_if_video()
 
--- window.py
+++ window.py
@@ -5,14 +5,16 @@ from tkinter import messagebox
 import os
 
 
-
 class Window:
     def __init__(self):
         self.root = tk.Tk()
         self.root.title("Youtube Downloader by KillSamurai!")
+
         self.URL = tk.StringVar()
         self.VIDEO_MP3 = tk.StringVar()
-        self.PERCENTAGE = "0%"
+        self.path_label = tk.StringVar()
+
+
         uri = tk.Label(self.root, text="YouTube URL: ")
         url = tk.Entry(self.root, textvariable=self.URL)
         self.saved_path = os.getcwd()
@@ -20,22 +22,23 @@ class Window:
         uri.grid(column=0, row=0, padx=10, pady=10)
         url.grid(column=1, row=0, padx=10, pady=10)
 
-        mp3 = tk.Radiobutton(self.root, text="Audio Only", variable=self.VIDEO_MP3, value=f"only_audio={True}")
-        video = tk.Radiobutton(self.root, text="Video", variable=self.VIDEO_MP3, value=f"only_video={True}")
+        video = tk.Radiobutton(self.root, text="Video", variable=self.VIDEO_MP3, value=False)
+        mp3 = tk.Radiobutton(self.root, text="Audio Only", variable=self.VIDEO_MP3, value=True)
 
-        mp3.grid(column=0, row=1, padx=10, pady=10)
-        # video.grid(column=1, row=1, padx=10, pady=10)
+        mp3.select()
+
+        mp3.grid(column=1, row=1, padx=10, pady=10)
+        video.grid(column=0, row=1, padx=10, pady=10)
+
+        self.path_label = tk.Entry(self.root, textvariable=self.path_label)
+        self.path_label.grid(column=0, row=2, padx=10, pady=10)
+        self.path_label.insert(0, self.saved_path)
 
         self.browse = tk.Button(self.root, text="Browse folder", command=lambda: self.where_to_save())
-        self.browse.grid(column=1, row=1, padx=10, pady=10)
-
-        percentage_label = tk.Label(self.root, text=self.PERCENTAGE)
-        # percentage_label.grid(column=0, row=2, pady=10, padx=10, columnspan=3)
+        self.browse.grid(column=1, row=2, padx=10, pady=10)
 
         button = tk.Button(self.root, text="LET'S GOOOO!!!!", command=lambda: self.check_mandatory())
         button.grid(column=0, row=3, padx=10, pady=10, columnspan=3)
-
-
 
         self.root.mainloop()
 
@@ -43,8 +46,8 @@ class Window:
         yt_url = self.URL.get()
         mp3_video = self.VIDEO_MP3.get()
         YouTube(url=yt_url, mp3=mp3_video, path=self.saved_path)
-
-        go_on = messagebox.askquestion(message="Continue?")
+        messagebox.showinfo(title="File Downloaded", message=f"File Downloaded at:\n {self.saved_path}")
+        go_on = messagebox.askquestion(message="A New Video to download?")
         if go_on == "no":
             self.root.destroy()
 
@@ -56,11 +59,4 @@ class Window:
 
     def where_to_save(self):
         self.saved_path = fd.askdirectory(parent=self.root, initialdir=self.saved_path, title="Choose where to save")
-
-
-
-
-
-
-
-
+        self.path_label.insert(0, self.saved_path)
